00ab31d16341610ba9c510172d6f7af6bbbd7cbf
leebumseok <qpwoeiru6486@gmail.com>
2017-09-05 15:05:50 +0900

    assignment

---
+++ 20171664-이범석-assignment2.py
@@ -0,0 +1,15 @@
+factorial = 1
+
+while 1:
+    n = int(input("Enter a number: "))
+    if n == -1:
+        break
+    elif n == 0:
+        print("%d! =" % n, factorial)
+    elif n < 0:
+        print("x")
+    else:
+        for i in range(1, n+1):
+            factorial *= i
+        print("%d! =" % n, factorial)
+        factorial = 1
